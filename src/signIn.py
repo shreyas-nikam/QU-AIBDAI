07468d06a2470c3cbf4ac4023cc1ee505d433c0d
Uddhav Zambare <158591657+uddhav-zambare@users.noreply.github.com>
2024-03-26 13:58:08 -0400

    Update signIn.py

--- src/signIn.py
+++ src/signIn.py
@@ -1,19 +1,24 @@
 import re
 import streamlit as st
 from supabase import create_client, Client
+import uuid
 
 class SignIn:
     def __init__(self):
         url: str = st.secrets["SUPABASE_URL"]
         key: str = st.secrets["SUPABASE_KEY"]
-        self.table_name: str = st.session_state.config_param["SUPABASE_TABLE_NAME"]
+        self.user_table_name: str = st.session_state.config_param["USER_TABLE_NAME"]
+
+        if "session_id" not in st.session_state:
+            st.session_state.session_id = str(uuid.uuid4())
+        
         if 'supabaseDB' not in st.session_state:
             supabase: Client = create_client(url, key)
             st.session_state.supabaseDB = supabase
     
     def validate_email(self,email):
         # Regular expression for basic email validation
-        pattern = r'^[a-zA-Z0-9_.+-]+@qusandbox\.com$'
+        pattern = r'^(?!\.)[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$'
         if re.match(pattern, email):
             return True
         else:
@@ -34,10 +39,8 @@ class SignIn:
         with cent_co:
             index = None
             st.markdown("<p style='text-align: center;'>Enter the following information to proceed.</p>", unsafe_allow_html=True)
-        # left_co, cent_co,right_co = st.columns(3)
-        # with cent_co:
             name = st.text_input("Name :", placeholder="Enter full name")
-            email = st.text_input("Email ID :", placeholder="Enter QuantUniversity email ID")
+            email = st.text_input("Email ID :", placeholder="Enter email address")
             agree = st.checkbox(f"""I read and agree to QuantUniversity's [Privacy Policy](https://www.quantuniversity.com/privacy.html)""")            
             
             st.write(' ')
@@ -50,14 +53,22 @@ class SignIn:
                 elif not email:
                     st.error("Please enter your email.")
                 elif not self.validate_email(email):
-                    st.error("Please enter a valid QuantUniversity email address.")
+                    st.error("Invalid email format. Please enter a valid email address.")
                 elif not agree:
                     st.error("Please read and agree the QuantUniversity's [Privacy Policy](https://www.quantuniversity.com/privacy.html).")
                 else:
-                    data, count = st.session_state.supabaseDB.table(self.table_name).insert({"name":name, "email":email}).execute()
-                    print(f"data {data[1][0]}")
+                    user_id  =  (str(uuid.uuid4()))[:8]
+                    user_data = {
+                        'user_id' : user_id,
+                        'session_id' : st.session_state.session_id,
+                        'name':name, 
+                        'email':email,
+                        'app_code':st.session_state.config_param["APP_CODE"]
+                    }
+                    data, count = st.session_state.supabaseDB.table(self.user_table_name).insert(user_data).execute()
+                    
                     st.session_state.user_info = data[1][0]
-                    print(f"st.session_state.user_info : {st.session_state.user_info['name']}")
+                    
                     st.rerun()
 
             cent_co1.markdown("<p style='text-align: center;'>Not a member? <a href='{url}'>SignUp</a></p>".format(url="https://academy.qusandbox.com/register"), unsafe_allow_html=True)
